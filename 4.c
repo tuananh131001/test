78afefc2624893af7ee1ef12a3a40b85cb963743
Tuan Anh <s3864077@rmit.edu.vn>
2020-07-22 20:54:18 +0700

    Perfect

--- 4.c
+++ 4.c
@@ -2,16 +2,20 @@
 int main()
 {
     char trash;
-    char names[5][50];
+    char first_name[5][32];
+    char last_name[5][32];
     for(int i=0;i<5;i++)
     {
         printf("Enter full name %d:",i+1);
-        scanf("%s",names[i]);
-        scanf("%c",&trash);
+        scanf("%s %[^\n]s",first_name[i],last_name[i]);
+
     }
+    printf("\n");
+    printf("Here are the first names.\n");
     for(int i=0;i<5;i++)
     {
-        printf("Here are the first name %d:",i+1,names[i]);
+        printf("%s, ",first_name[i]);
+
     }
 
     return 0;
