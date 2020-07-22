3d905b3ac812122b7fb4acc92ae4ca9c7fadf6b4
Tuan Anh <s3864077@rmit.edu.vn>
2020-07-22 21:00:33 +0700

    make some comment

--- 4.c
+++ 4.c
@@ -4,15 +4,17 @@ int main()
     char trash;
     char first_name[5][32];
     char last_name[5][32];
+    //last name include middlename + last name
     for(int i=0;i<5;i++)
     {
         printf("Enter full name %d:",i+1);
         scanf("%s %[^\n]s",first_name[i],last_name[i]);
 
     }
+    //make a new line
     printf("\n");
     printf("Here are the first names.\n");
-    for(int i=0;i<5;i++)
+    for(int i=0;i<5;i++)//loop to print all 5 first name
     {
         printf("%s, ",first_name[i]);
 
